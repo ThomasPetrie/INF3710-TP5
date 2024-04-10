6bba6de8180b1ad813e57ac0fa374ddbcc2f3679
RosemarieMillette <rosemarie.millette@polymtl.ca>
2024-04-10 14:56:22 -0400

    statut

--- server/app/services/database.service.ts
+++ server/app/services/database.service.ts
@@ -9,7 +9,7 @@ export class DatabaseService {
   public connectionConfig: pg.ConnectionConfig = {
     user: "postgres",
     database: "ornithologue_bd",
-    password: "caniche20",
+    password: "",
     port: 5432,
     host: "127.0.0.1",
     keepAlive: true,
